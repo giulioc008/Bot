2fd3822e2f4126f3e8d83118b34821213841a3f1
Giulio Coa <34110430+giulioc008@users.noreply.github.com>
2020-03-14 23:15:16 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-import json
 import logging as logger
 import re
 
@@ -18,20 +17,11 @@ commands = list(["addadmin",
 				 "start"
 				])
 constants = Constants.Constants()
-initialLog = list(["Initializing the Admins ...", "Admins initializated\nSetting the admins list ...",
-				   "Admins setted\nSetting the users list ...", "Users initializated\nInitializing the Client ..."])
 logger.basicConfig(filename="{}{}.log".format(constants.databasePath, constants.username), datefmt="%d/%m/%Y %H:%M:%S", format="At %(asctime)s was logged the event:\t%(levelname)s - %(message)s", level=logger.INFO)
-for i in initialLog:
-    logger.info(i)
 scheduler = schedule.default_scheduler
-"""
-	Initializing the Admins ...
-"""
+logger.info("Initializing the Admins ...")
 constants.loadCreators()
-"""
-	Admins initializated
-	Setting the admins list ...
-"""
+logger.info("Admins initializated\nSetting the admins list ...")
 adminsIdList = set()
 i = constants.admins.to_json(orient="columns")
 i = i[len("{\"id\":{"):i.index("}")]
@@ -41,10 +31,7 @@ i = list(map(lambda n: dict({n[0]: n[1]}), i))
 i = list(map(lambda n: list(n.values()), i))
 list(map(lambda n: list(map(lambda m: adminsIdList.add(int(m)), n)), i))
 adminsIdList = list(adminsIdList)
-"""
-	Admins setted
-	Setting the users list ...
-"""
+logger.info("Admins setted\nSetting the users list ...")
 userIdList = set()
 i = constants.users.to_json(orient="columns")
 i = i[len("{\"id\":{"):i.index("}")]
@@ -54,24 +41,19 @@ i = list(map(lambda n: dict({n[0]: n[1]}), i))
 i = list(map(lambda n: list(n.values()), i))
 list(map(lambda n: list(map(lambda m: userIdList.add(int(m)), n)), i))
 userIdList = list(userIdList)
-"""
-	Users initializated
-	Initializing the Client ...
-"""
+logger.info("Users initializated\nInitializing the Client ...")
 app = Client(session_name=constants.username, api_id=constants.id, api_hash=constants.hash, bot_token=constants.token)
 
 
-@app.on_message(
-		Filters.command("addadmin", prefixes=list(["/"])) & Filters.user(adminsIdList) & Filters.private)
+@app.on_message(Filters.command("addadmin", prefixes=list(["/"])) & Filters.user(adminsIdList) & Filters.private)
 def addAdmin(client: Client, message: Message):
-    """
-        /addadmin [username]
-    """
-	global adminsIdList, constants
+	"""
+        	/addadmin [username]
+	"""
+	global adminsIdList
 
 	user = client.get_users(message.command.pop(1))
-	text = "The user {}".format("{} ".format(user.first_name) if user.first_name is not None else "")
-	text += "{}is already present in the list of allowed chat.".format("{} ".format(user.last_name) if user.last_name is not None else "")
+	text = "@{} is already present in the admin list.".format(user.username))
 	if user.id not in adminsIdList:
 		"""
 			Adding the chat to the database
@@ -106,17 +88,12 @@ def addAdmin(client: Client, message: Message):
 		except KeyError:
 			pass
 		constants.admins = list([userDict])
-		text = "I added {}".format("{} ".format(user.first_name) if user.first_name is not None else "")
-		text += "{}to the list of allowed chat at {}.".format("{} ".format(user.last_name) if user.last_name is not None else "", constants.now())
-	log(client, text)
+		text = "I added @{} to the admins database.".format(user.username)
     logger.info(text)
 
 
 @app.on_callback_query(Filters.user(userIdList))
-def answerInlineButton(client: Client, callback_query: CallbackQuery):
-    global constants
-
-	log(client, "@{} has pressed an Inline button at {}.".format(callback_query.from_user.username, constants.now()))
+def answerInlineButton(_, callback_query: CallbackQuery):
 	keyboard = list()
 	text = ""
 	if callback_query.data.lower() == "text":
@@ -140,88 +117,74 @@ def answerInlineButton(client: Client, callback_query: CallbackQuery):
 	callback_query.answer(text, show_alert=True)
 	callback_query.edit_message_text(text, disable_web_page_preview=True)
 	callback_query.edit_message_reply_markup(InlineKeyboardMarkup(keyboard))
-	log(client, "I have answered to an Inline button at {}.".format(constants.now()))
-    logger.info("I have answered to an Inline button at {}.".format(constants.now()))
+	logger.info("I have answered to an Inline button.")
 
 
 @app.on_message(Filters.service)
-def automaticRemovalStatus(client: Client, message: Message):
-	"""
-		Removing the status message
-	"""
+def automaticRemovalStatus(_, message: Message):
 	message.delete(revoke=True)
 
 
 @app.on_message(
 		Filters.command("command1", prefixes=list(["/"])) & Filters.user(userIdList) & Filters.private)
 def command1(client: Client, message: Message):
-    global constants
-
 	"""
 		If the command has any arguments, it can be acceded at message.command parameter
-		That parameter is a list with the first element equal to the command (message.command(0) == "command1")
+		That parameter is a list with the first element equal to the command (message.command[0] == "command1")
 	"""
-	log(client, "I have answered to /command1 at {} because of @{}.".format(constants.now(), message.from_user.username))
-    logger.info("I have answered to /command1 at {} because of @{}.".format(constants.now(), message.from_user.username))
+	logger.info("I have answered to /command1 because of @{}.".format(message.from_user.username))
 
 
 @app.on_message(
 		Filters.command("command2", prefixes=list(["/"])) & Filters.user(userIdList) & Filters.private)
-def command2(client: Client, message: Message):
-    """
-        /command2
-    """
-    global constants, scheduler
+def command2(_, message: Message):
+    	"""
+        	/command2
+    	"""
+    	global constants, scheduler
 
 	for i in range(constants.users.shape[0]):
 		if constants.users.at[i, "id"] == message.from_user.id and constants.users.at[i, "flag"] is False:
 			scheduler.every().day.at("14:00").do(queue1, client=client, ...)
-			log(client, "@{} activated the Job Queue that DO SOMETHING at {}.".format(message.from_user.username, constants.now()))
+			logger.info("@{} activated the Job Queue that DO SOMETHING.".format(message.from_user.username))
 			scheduler.every().monday.at("2:00").do(queue2, client=client, ...)
-			log(client, "@{} activated the Job Queue that DO SOMETHING ELSE at {}.".format(message.from_user.username, constants.now()))
+			logger.info("@{} activated the Job Queue that DO SOMETHING ELSE.".format(message.from_user.username))
 
 			...
 
 			constants.users.at[i, "flag"] = True
 			break
-	log(client, "I have answered to /command2 at {} because of @{}.".format(constants.now(), message.from_user.username))
-    logger.info("I have answered to /command2 at {} because of @{}.".format(constants.now(), message.from_user.username))
+	logger.info("I have answered to /command2 ecause of @{}.".format(message.from_user.username))
 
 
 @app.on_message(
 		Filters.command("command3", prefixes=list(["/"])) & Filters.user(userIdList) & Filters.private)
-def command3(client: Client, message: Message):
-    """
-        Command that reply with a keyboard
-    """
-    global constants
-
+def command3(_, message: Message):
+    	"""
+        	Command that reply with a keyboard
+    	"""
 	keyboard = ReplyKeyboardMarkup(keyboard=list([list([KeyboardButton("Text"), ...]), ...]), resize_keyboard=True, one_time_keyboard=False)
 	message.reply_text("Text", reply_markup=keyboard)
-	log(client, "I have answered to /command3 at {} because of @{}.".format(constants.now(), message.from_user.username))
-    logger.info("I have answered to /command3 at {} because of @{}.".format(constants.now(), message.from_user.username))
+	logger.info("I have answered to /command3 because of @{}.".format(message.from_user.username))
 
 
 @app.on_message(
 		Filters.command("help", prefixes=list(["/"])) & Filters.user(userIdList) & Filters.private)
-def help(client: Client, message: Message):
-    """
-        /help
-    """
-    global commands, constants
+def help(_, message: Message):
+    	"""
+        	/help
+    	"""
+    	global commands
 
 	message.reply_text("In this section you will find the list of the command of the bot.\n\t{}.".format("\n\t".join(commands)))
-	log(client, "I helped @{} at {}.".format(message.from_user.username, constants.now()))
-    logger.info("I helped @{} at {}.".format(message.from_user.username, constants.now()))
+	logger.info("I helped @{}.".format(message.from_user.username))
 
 
 @app.on_inline_query(Filters.user(userIdList))
-def inline(client: Client, inline_query: InlineQuery):
-    """
-        Inline command
-    """
-    global constants
-
+def inline(_, inline_query: InlineQuery):
+    	"""
+        	Inline command
+    	"""
 	results = list()
 	keyboard = list()
 	queryID = ""
@@ -277,64 +240,40 @@ def inline(client: Client, inline_query: InlineQuery):
 		Sending the output
 	"""
 	inline_query.answer(results, switch_pm_text="Text", switch_pm_parameter="Text")
-	log(client, "I sent the answer to the Inline Query of @{}.".format(inline_query.from_user.username))
-    logger.info("I sent the answer to the Inline Query of @{}.".format(inline_query.from_user.username))
-
-
-def log(client: Client = None, logging: str = ""):
-	global constants, initialLog
-
-	if client is not None:
-		if initialLog is not None:
-			for msg in initialLog:
-				client.send_message(constants.log, msg)
-			initialLog = None
-		client.send_message(constants.log, logging)
-	else:
-		initialLog.append(logging)
+	logger.info("I sent the answer to the Inline Query of @{}.".format(inline_query.from_user.username))
 
 
 def queue1(client: Client, ...):
-    global constants
-
-    """
-        Do a Job in the Job Queue
-    """
-	log(client, "I have done my job at {}.".format(constants.now()))
-    logger.info("I have done my job at {}.".format(constants.now()))
+    	"""
+        	Do a Job in the Job Queue
+    	"""
+	logger.info("I have done my job.")
 
 
 def queue2(client: Client, ...):
-    global constants
-
-    """
-        Do a Job in the Job Queue
-    """
-	log(client, "I have done my job at {}.".format(constants.now()))
-    logger.info("I have done my job at {}.".format(constants.now()))
+    	"""
+        	Do a Job in the Job Queue
+    	"""
+	logger.info("I have done my job.")
 
 
 @app.on_message(
 		Filters.command("removeadmin", prefixes=list(["/"])) & Filters.user(adminsIdList) & Filters.private)
 def removeAdmin(client: Client, message: Message):
-    """
-        /removeadmin [username]
-    """
-    global constants
-
-	message.command.pop(0)
+    	"""
+        	/removeadmin [username]
+    	"""
+    	message.command.pop(0)
 	if len(message.command) != 1:
 		message.reply_text("The syntax is: `/removeadmin [username]`.")
-		log(client,
-			"I helped @{} with /removeadmin at {}.".format(message.from_user.username, constants.now()))
-		logger.info("I helped @{} with /removeadmin at {}.".format(message.from_user.username, constants.now()))
+		logger.info("I helped @{} with /removeadmin.".format(message.from_user.username))
 		return
 	for i in range(constants.admins.shape[0]):
 		if constants.admins.at[i, "username"].lower() == message.command[0].lower():
 			if constants.creator == constants.admins.at[i, "id"]:
 				message.reply_text("You can\'t remove the creator of the bot from the admin list.")
-				log(client, "@{} tried to remove you as admin at {}.".format(message.from_user.username, constants.now()))
-				logger.info("@{} tried to remove you as admin at {}.".format(message.from_user.username, constants.now()))
+				client.send_message(constants.creator, "@{} have tried to remove you as admin.".format(message.from_user.username))
+				logger.info("@{} tried to remove you as admin.".format(message.from_user.username))
 				return
 			else:
 				message.command[0] = constants.admins.at[i, "username"]
@@ -343,24 +282,20 @@ def removeAdmin(client: Client, message: Message):
 	constants.admins.reset_index(drop=True)
 	constants.save()
 	message.reply_text("Admin removed.")
-	log(client, "I removed an admin (@{}) at @{}\'s request at {}.".format(message.command[0], message.from_user.username, constants.now()))
-    logger.info("I removed an admin (@{}) at @{}\'s request at {}.".format(message.command[0], message.from_user.username, constants.now()))
+	logger.info("I removed @{} from the admin database.".format(message.command[0]))
 
 
 @app.on_message(
 		Filters.command("report", prefixes=list(["/"])) & Filters.user(constants.creator) & Filters.private)
-def report(client: Client, message: Message):
-    """
-        /report
-    """
-    global commands, constants
+def report(_, message: Message):
+    	"""
+        	/report
+    	"""
+    	global commands
 
-	text = commands.copy()
-	for i in text:
-		i += " - Description"
+	text = list(map(lambda n: "{} - Description".format(n), commands))
 	message.reply_text("\n".join(text))
-	log(client, "I send a report to @{} at {}.".format(message.from_user.username, constants.now()))
-    logger.info("I send a report to @{} at {}.".format(message.from_user.username, constants.now()))
+	logger.info("I send a report.")
 
 
 @app.on_message(Filters.text & Filters.user(userIdList) & Filters.private)
@@ -379,15 +314,12 @@ def split(client: Client, message: Message):
 
 @app.on_message(
 		Filters.command("start", prefixes=list(["/"])) & Filters.user(userIdList) & Filters.private)
-def start(client: Client, message: Message):
-    """
-        /start
-    """
-    global constants
-
-	message.reply_text("Welcome @{}.\nThis bot ...".format(message.from_user.username))
-	log(client, "I started at {} because of @{}.".format(constants.now(), message.from_user.username))
-    logger.info("I started at {} because of @{}.".format(constants.now(), message.from_user.username))
+def start(_, message: Message):
+    	"""
+        	/start
+    	"""
+    	message.reply_text("Welcome @{}.\nThis bot ...".format(message.from_user.username))
+	logger.info("I started because of @{}.".format(message.from_user.username))
 
 
 def unknownFilter():
@@ -405,19 +337,13 @@ def unknownFilter():
 
 @app.on_message(unknownFilter() & Filters.user(userIdList) & Filters.private)
 def unknown(client: Client, message: Message):
-	global constants
-
 	message.reply_text("This command isn\'t supported.")
-	log(client, "I managed an unsupported command at {}.".format(constants.now()))
-    logger.info("I managed an unsupported command at {}.".format(constants.now()))
+    	logger.info("I managed an unsupported command.")
 
 
-log(logging="Client initializated\nSetting the markup syntax ...")
 logger.info("Client initializated\nSetting the markup syntax ...")
 app.set_parse_mode("html")
-log(logging="Setted the markup syntax\nSetting the Job Queue ...")
 logger.info("Setted the markup syntax\nSetting the Job Queue ...")
-log(logging="Setted the Job Queue\nStarted serving ...")
 logger.info("Setted the Job Queue\nStarted serving ...")
 with app:
 	while True:
